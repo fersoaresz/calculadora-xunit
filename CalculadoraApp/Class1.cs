659f8c5d29b22fd5090833104c100f499591a674
Fernando Ferreira <fernan_fer_soar@hotmail.com>
2025-04-16 00:19:13 -0300

    calculadora com testes

---
+++ CalculadoraApp/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraApp;
+
+public class Calculadora
+{
+    public double Somar(double a, double b) => a + b;
+    public double Subtrair(double a, double b) => a - b;
+    public double Multiplicar(double a, double b) => a * b;
+    public double Dividir(double a, double b) => b != 0 ? a / b : throw new DivideByZeroException();
+    public double Potenciar(double a, double b) => Math.Pow(a, b);
+}
---
+++ CalculadoraTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using CalculadoraApp;
+
+namespace CalculadoraTest;
+
+public class CalculadoraTests
+{
+    Calculadora calc = new Calculadora();
+
+    [Fact]
+    public void TesteSomar() => Assert.Equal(5, calc.Somar(2, 3));
+
+    [Fact]
+    public void TesteSubtrair() => Assert.Equal(1, calc.Subtrair(4, 3));
+
+    [Fact]
+    public void TesteMultiplicar() => Assert.Equal(15, calc.Multiplicar(3, 5));
+
+    [Fact]
+    public void TesteDividir() => Assert.Equal(2, calc.Dividir(10, 5));
+
+    [Fact]
+    public void TestePotenciar() => Assert.Equal(16, calc.Potenciar(2, 4));
+
+    [Fact]
+    public void TesteDivisaoPorZero() =>
+        Assert.Throws<DivideByZeroException>(() => calc.Dividir(10, 0));
+}
